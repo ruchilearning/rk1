ce5da91fe350a8dee13d33f73afaa5e0e548a255
ruchi <ruchithak@outlook.com>
2022-11-06 20:26:51 +1100

    working test endpoint

--- src/main/java/com/rk1/RkApplication.java
+++ src/main/java/com/rk1/RkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Rk1Application {
+public class RkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Rk1Application.class, args);
+		SpringApplication.run(RkApplication.class, args);
 	}
 
 }
