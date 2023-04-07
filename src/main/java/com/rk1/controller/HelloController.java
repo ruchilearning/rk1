0add4d04cf855bcd72c5758f4b27225e7454fafd
ruchi <ruchithak@outlook.com>
2023-04-07 16:24:04 +1000

    Flux and Mono

--- src/main/java/com/rk1/controller/HelloController.java
+++ src/main/java/com/rk1/controller/HelloController.java
@@ -4,13 +4,19 @@ import com.rk1.configs.KafkaConfigProperties;
 import com.rk1.repository.HelloRepository;
 import com.rk1.service.HelloService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value="hello")
 public class HelloController {
 
     private final KafkaConfigProperties kafkaConfigProperties;
@@ -18,12 +24,36 @@ public class HelloController {
 
     @GetMapping(value="/hello")
     @ResponseBody
-    public Mono<HelloRepository.HelloResponse> bootup()
+    public Mono<HelloRepository.HelloResponse> bootUp()
     {
 
 //        return "SpringBoot is up and running";
 //        return kafkaConfigProperties.toString();
         return helloService.callHello();
+
+    }
+
+    @GetMapping("/flux")
+    public Flux<Integer> flux(){
+
+        return Flux.just(1,2,3)
+                .log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> helloWorldMono(){
+
+        return Mono.just("hello-world")
+                .log();
+
+    }
+
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream(){
+
+        return Flux.interval(Duration.ofSeconds(1))
+                .log();
 
     }
 }
