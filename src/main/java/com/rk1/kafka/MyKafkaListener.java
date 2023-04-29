efd731d283fa3c867680fbb7204783b3c2d170d9
ruchi <ruchithak@outlook.com>
2023-04-29 10:31:41 +1000

    add info logs

--- src/main/java/com/rk1/kafka/MyKafkaListener.java
+++ src/main/java/com/rk1/kafka/MyKafkaListener.java
@@ -3,6 +3,7 @@ package com.rk1.kafka;
 import com.rk1.service.MyListenerService;
 import com.rk5.avro01.Avro01;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MyKafkaListener {
 
     private final KafkaProducer kafkaProducer;
@@ -17,7 +19,7 @@ public class MyKafkaListener {
 
     @KafkaListener(topics = "test1", groupId = "my-group", containerFactory = "kafkaListenerContainerFactory")
     public void onMessage(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
-        System.out.println("Received message: " + record.value());
+        log.info("Received message: {}", record.value());
         kafkaProducer.sendMessage("test2", record.value());
         acknowledgment.acknowledge();
     }
@@ -25,10 +27,7 @@ public class MyKafkaListener {
     @KafkaListener(topics = "rk-avro01", groupId = "my-group-avro", containerFactory = "kafkaListenerContainerFactoryAvro")
     public void onAvroMessage(ConsumerRecord<String, Avro01> record, Acknowledgment acknowledgment) {
         Avro01 value = record.value();
-
-        System.out.println("Received key: " + record.key());
-        System.out.println("Received message: " + value);
-
+        log.info("Received key: {}, Received message: {}", record.key(), value);
         myListenerService.processKafkaMessage(value);
         acknowledgment.acknowledge();
     }
